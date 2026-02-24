ce8c472f0d31ac5ad4c4680d1017467ba7658121
Dalia1199 <dalia.shabana1199@gmail.com>
2026-02-24 03:56:57 +0200

    replace login.js on main with updated

---
+++ js/login-page.js
@@ -0,0 +1,31 @@
+
+import { getUsers, updateCurrentUser } from "./utils.js";
+
+const form = document.getElementById("registrationForm");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let users = getUsers();
+
+    let foundUser = users.find(user =>
+        user.email === email.value.trim() && user.password === password.value.trim()
+    );
+
+    if (foundUser) {
+        updateCurrentUser(foundUser);
+
+        alert("Login Successful");
+
+        if (foundUser.hasTakenQuiz) {
+            window.location.href = "results.html"; 
+        } else {
+            window.location.href = "quiz.html"; 
+        }
+
+    } else {
+        alert("Email or Password incorrect");
+    }
+}); 
